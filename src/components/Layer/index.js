678218ef54d3ee1d60ab6e0a84725a948357082d
etd2w <etd2wjd@gmail.com>
2021-02-08 20:40:14 +0400

    Context menu for layers

--- src/components/Layer/index.js
+++ src/components/Layer/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import ShapeBar from "../ShapeBar";
 import styles from "./style.module.css";
 
@@ -6,6 +7,8 @@ export default function Layer({ item }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isAddingChild, setIsAddingChild] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const dispatch = useDispatch();
 
   let children = null;
 
@@ -22,6 +25,18 @@ export default function Layer({ item }) {
       </div>
     );
   }
+
+  const handleRemove = () => {
+    item.remove();
+
+    dispatch({ type: "REMOVE_LAYER", payload: item });
+  };
+
+  const handleVisible = () => {
+    setIsVisible(!isVisible);
+
+    item.visible = !item.visible;
+  };
 
   const handleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -49,24 +64,25 @@ export default function Layer({ item }) {
       <div className={styles.layer}>
         <div
           className={
-            item.children.length && item.children[0].id
+            item.children.length && item.children[item.children.length - 1].id
               ? styles.leftHalf
               : styles.leftPad
           }
         >
-          {item.children.length > 0 && item.children[0].id && (
-            <button className={styles.btnExpand} onClick={handleExpand}>
-              <svg
-                width="6"
-                height="7"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                transform={isExpanded ? "rotate(90)" : ""}
-              >
-                <path d="M5.5 3.5l-5 3v-6l5 3z" fill="#fff" />
-              </svg>
-            </button>
-          )}
+          {item.children.length > 0 &&
+            item.children[item.children.length - 1].id && (
+              <button className={styles.btnExpand} onClick={handleExpand}>
+                <svg
+                  width="6"
+                  height="7"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  transform={isExpanded ? "rotate(90)" : ""}
+                >
+                  <path d="M5.5 3.5l-5 3v-6l5 3z" fill="#fff" />
+                </svg>
+              </button>
+            )}
           <span>{item.type}</span>
         </div>
 
@@ -102,8 +118,10 @@ export default function Layer({ item }) {
         {isMenuOpen && (
           <div className={styles.layerMenu}>
             <button>Copy the element</button>
-            <button>Hide the element</button>
-            <button>Remove the element</button>
+            <button onClick={handleVisible}>
+              {isVisible ? "Hide the element" : "Show the element"}
+            </button>
+            <button onClick={handleRemove}>Remove the element</button>
           </div>
         )}
 
--- src/redux/redusers/layersReducer.js
+++ src/redux/redusers/layersReducer.js
@@ -2,6 +2,10 @@ export default function layersReducer(state = [], action) {
   switch (action.type) {
     case "ADD_LAYER":
       return [...state, action.payload];
+    case "REMOVE_LAYER":
+      const newState = [...state];
+      newState.splice(newState.indexOf(action.payload), 1);
+      return newState;
     default:
       return state;
   }
